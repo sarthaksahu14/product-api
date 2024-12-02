1d370ed8327f144a91925a8aec602fc0235244b3
Sarthak Sahu <14sarthak.sahu@gmail.com>
2024-12-02 16:11:46 +0530

    Added CORS support

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require('cors'); 
 const products_routes = require("./routes/products");
 const connectDB = require("./db/connect");
+
 const app = express();
 const port = process.env.port || 3005;
+
+app.use(cors());  
 
 app.use(express.json());
 
@@ -13,13 +17,8 @@ app.get('/', (req, res) => {
 
 app.use("/products", products_routes);
 
-app.listen(port,async () => {
- await connectDB(process.env.MONGODB_URL);
+app.listen(port, async () => {
+  await connectDB(process.env.MONGODB_URL);
   console.log(`Server running on http://localhost:${port}`);
-});   
-
-
-
-
-
+});
 
